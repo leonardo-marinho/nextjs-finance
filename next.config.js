e65689923ddb09552794a24a368aed94eaa11bcf
leonardo-marinho <leonardo.msnt@outlook.com>
2023-12-21 18:44:28 -0300

    Add polyfills to webpack entry point

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+
+      if (entries['main.js'] && !entries['main.js'].includes('./src/lib/polyfills.ts')) {
+        entries['main.js'].unshift('./src/lib/polyfills.ts');
+      }
+
+      return entries;
+    };
+
+    return config;
+  },
+};
 
 module.exports = nextConfig;

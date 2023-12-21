5406c118a911fa6f42b8ddc5e0586dde4b6ea5ed
leonardo-marinho <leonardo.msnt@outlook.com>
2023-12-21 19:17:20 -0300

    Add User table and index in migration.sql and update User model in schema.prisma

--- prisma/migrations/20231221220857_/migration.sql
+++ prisma/migrations/20231221220857_/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT,
+    "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3),
     "deletedAt" BOOLEAN NOT NULL DEFAULT false,

7ee3f6f553329ec5e53b02f2061aa7a4a993690a
Leonardo Marinho <50488688+leonardo-marinho@users.noreply.github.com>
2024-01-02 00:05:31 -0300

    Revert "UI // Add textinput component (#52)" (#53)
    This reverts commit 400f16a6c73d3f630b21c3b8b4677159619fe5df.

--- src/client/components/UI/Spinner/Spinner.tsx
+++ src/client/components/UI/Spinner/Spinner.tsx
@@ -30,7 +30,6 @@ export const Spinner: React.FC<SpinnerProps> = ({
         className="opacity-25"
         cx="12"
         cy="12"
-        opacity={0.25}
         r="10"
         stroke="currentColor"
         stroke-width="4"
@@ -39,7 +38,6 @@ export const Spinner: React.FC<SpinnerProps> = ({
         className="opacity-75"
         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
         fill="currentColor"
-        fillOpacity={0.5}
       ></path>
     </svg>
   );

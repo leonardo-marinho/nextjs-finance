ee9d4aacb4204c44b36fbcdef9801720257836b3
Leonardo Marinho <50488688+leonardo-marinho@users.noreply.github.com>
2024-01-02 00:07:20 -0300

    UI // Add textinput component (#54)
    * Add react-hook-form dependency
    
    * Add opacity and fillOpacity props to Spinner component
    
    * Add Input component and Input story
    
    * Refactor code to improve performance and readability
    
    * Add InputWrapper and TextInput components
    
    * Update title in InputWrapper.stories.tsx

---
+++ src/client/components/UI/inputs/InputWrapper/InputWrapper.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { InputWrapper } from './InputWrapper';
+
+const meta: Meta<typeof InputWrapper> = {
+  component: InputWrapper,
+  tags: ['autodocs'],
+  title: 'Components/UI/Inputs/InputWrapper',
+};
+
+export default meta;
+type Story = StoryObj<typeof InputWrapper>;
+
+export const Default: Story = {
+  args: {
+    label: 'InputWrapper',
+  },
+};
---
+++ src/client/components/UI/inputs/InputWrapper/InputWrapper.tsx
@@ -0,0 +1,36 @@
+import classNames from 'classnames';
+import { FieldError } from 'react-hook-form';
+
+export interface InputWrapperProps {
+  error?: FieldError;
+  label: string;
+  maxLength?: number;
+  required?: boolean;
+  valueLength?: number;
+}
+
+export const InputWrapper: React.FC<React.PropsWithChildren<InputWrapperProps>> = ({
+  children,
+  error,
+  label,
+  maxLength,
+  required,
+  valueLength,
+}): JSX.Element => {
+  return (
+    <div>
+      <label className="field-label mt-1 flex-1 justify-self-end text-base text-gray-500">
+        {label} {required && '*'}
+      </label>
+      {children}
+      <div className={classNames('flex justify-between text-right')}>
+        {error && <p className="ml-1 mr-5 mt-1 text-sm text-red-600">{error.message}</p>}
+        {maxLength && maxLength > 0 && (
+          <p className="mt-1 flex-1 justify-self-end text-xs text-gray-500">{`${
+            valueLength || 0
+          }/${maxLength}`}</p>
+        )}
+      </div>
+    </div>
+  );
+};
